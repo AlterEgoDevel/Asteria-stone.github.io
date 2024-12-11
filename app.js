6c4709d7a3a180a56f0e7313781d68209afda412
AlterEgoDevel <AlterEgoDevel@gmail.com>
2024-12-11 16:29:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-if (document.documentElement.requestFullscreen) {
-    document.documentElement.requestFullscreen();
-} else if (document.documentElement.mozRequestFullScreen) {
-    document.documentElement.mozRequestFullScreen();
-} else if (document.documentElement.webkitRequestFullscreen) {
-    document.documentElement.webkitRequestFullscreen();
+   // Функция для перехода в полноэкранный режим
+   function enterFullScreen() {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen(); // Firefox
+    } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen(); // Chrome, Safari
+    }
 }
+
+// Вызов функции
+enterFullScreen();
 
